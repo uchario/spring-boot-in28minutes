90d644fbacdaf94a43b75a75fc29e99a2e5a3547
uchario <uchario@yahoo.com>
2024-11-03 00:13:09 +0100

    Exploring internationalization for REST API

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/controller/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.in28minutes.rest.webservices.restful_web_services.controller;
 
 import com.in28minutes.rest.webservices.restful_web_services.repository.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("hello-world")
     public String helloworld() {
         return "Hello World";
@@ -20,5 +28,11 @@ public class HelloWorldController {
     @GetMapping("hello/{name}")
     public String getName(@PathVariable String name) {
         return name;
+    }
+
+    @GetMapping("/hello-world-int")
+    public String helloWorldInt() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
