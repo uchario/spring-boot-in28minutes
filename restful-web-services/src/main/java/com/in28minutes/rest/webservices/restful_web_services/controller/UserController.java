d5b2080afe3e99086a19ca4b1989714fbdc5a94a
uchario <uchario@yahoo.com>
2024-11-03 01:48:21 +0100

    implementing HATEOAS for REST API

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/controller/UserController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/controller/UserController.java
@@ -5,9 +5,12 @@ import com.in28minutes.rest.webservices.restful_web_services.repository.User;
 import com.in28minutes.rest.webservices.restful_web_services.service.UserDaoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import java.net.URI;
 import java.util.List;
@@ -23,12 +26,15 @@ public class UserController {
     }
 
     @GetMapping("users/{id}")
-    public User getUser(@PathVariable Integer id) {
+    public EntityModel<User> getUser(@PathVariable Integer id) {
         User user = userDaoService.findById(id);
         if(user == null) {
             throw new UserNotFoundException("id: " + id);
         }
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).getUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
     }
 
     @PostMapping("users")
