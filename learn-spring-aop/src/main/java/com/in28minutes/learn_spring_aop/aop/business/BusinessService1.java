e9c5467d32979747c7e19116c1c37b1d93126c39
uchario <uchario@yahoo.com>
2024-11-15 00:54:35 +0100

    Setting up Spring Components for Spring AOP

---
+++ learn-spring-aop/src/main/java/com/in28minutes/learn_spring_aop/aop/business/BusinessService1.java
@@ -0,0 +1,18 @@
+package com.in28minutes.learn_spring_aop.aop.business;
+
+import com.in28minutes.learn_spring_aop.aop.data.DataService1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+@Service
+public class BusinessService1 {
+    @Autowired
+    private DataService1 dataService1;
+
+    public int calculateMax() {
+        int[] data = dataService1.retrieveData();
+        return Arrays.stream(data).max().orElse(0);
+    }
+}
