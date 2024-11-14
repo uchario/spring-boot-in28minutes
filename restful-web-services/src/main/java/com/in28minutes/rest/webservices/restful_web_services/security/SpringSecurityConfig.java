a3c145dfcd9610d9e76f66cb9fbb6986d6c8fd9c
uchario <uchario@yahoo.com>
2024-11-14 23:34:40 +0100

    Enhancing Spring Security Configuration for Basic Authentication

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/security/SpringSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.in28minutes.rest.webservices.restful_web_services.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated()
+        );
+        http.httpBasic(withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
