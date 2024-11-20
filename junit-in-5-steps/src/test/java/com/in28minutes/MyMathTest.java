703c58bbb492a05d30b25dfffceaee1f01feab26
uchario <uchario@yahoo.com>
2024-11-20 23:33:56 +0100

    Exploring other Assert Methods

--- junit-in-5-steps/src/test/java/com/in28minutes/MyMathTest.java
+++ junit-in-5-steps/src/test/java/com/in28minutes/MyMathTest.java
@@ -2,10 +2,16 @@ package com.in28minutes;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyMathTest {
     private final MyMath myMath = new MyMath();
+    private final List<String> todos = Arrays.asList("AWS", "Azure", "DevOps");
+
     @Test
     void calculateSum_ThreeLengthArray() {
         assertEquals(6, myMath.calculateSum(new int[]{1,2,3}));
@@ -14,5 +20,15 @@ class MyMathTest {
     @Test
     void calculateSum_ZeroLengthArray() {
         assertEquals(0, myMath.calculateSum(new int[]{}));
+    }
+
+    @Test
+    void isPresentInList() {
+        assertTrue(todos.contains("AWS"));
+    }
+
+    @Test
+    void isAbsentInList() {
+        assertFalse(todos.contains("GCP"));
     }
 }
