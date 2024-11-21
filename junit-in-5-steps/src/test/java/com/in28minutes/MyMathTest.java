e321bf776b75a3c15f058a4982618ce6b054ebba
uchario <uchario@yahoo.com>
2024-11-21 07:45:54 +0100

    Exploring few important JUnit Annotations

--- junit-in-5-steps/src/test/java/com/in28minutes/MyMathTest.java
+++ junit-in-5-steps/src/test/java/com/in28minutes/MyMathTest.java
@@ -1,6 +1,7 @@
 package com.in28minutes;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.w3c.dom.ls.LSOutput;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,6 +13,15 @@ class MyMathTest {
     private final MyMath myMath = new MyMath();
     private final List<String> todos = Arrays.asList("AWS", "Azure", "DevOps");
 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
     @Test
     void calculateSum_ThreeLengthArray() {
         assertEquals(6, myMath.calculateSum(new int[]{1,2,3}));
@@ -30,5 +40,15 @@ class MyMathTest {
     @Test
     void isAbsentInList() {
         assertFalse(todos.contains("GCP"));
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All");
     }
 }
